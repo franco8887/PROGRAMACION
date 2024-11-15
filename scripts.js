da5dbd3177a6b4acc77d6e5f7529b842ec3771fb
Marcos Rossi <101893923+gurrucha@users.noreply.github.com>
2024-11-15 13:01:54 -0300

    cambios

--- scripts.js
+++ scripts.js
@@ -4,3 +4,14 @@ function generateRandomNumber(min, max) {
 }
 
 console.log(generateRandomNumber(1, 100));
+function generateRandomString(length) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
+console.log(generateRandomString(10));
